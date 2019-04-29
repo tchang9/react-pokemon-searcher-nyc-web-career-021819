dad287368a7a151f009b8bd312ac417507dba6ab
tchang9 <tlchang@gmail.com>
2019-04-28 23:46:54 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,31 +2,58 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCollection extends React.Component {
+// class PokemonCollection extends React.Component {
+
+//   // takes an array of pokemons and renders 
+//   renderAllPokemon = () => {
+//     return this.filteredPokemons().map(pokemon => {
+//       return <PokemonCard pokemon={pokemon}/>
+//     })
+//   }
+
+//   // an array of pokemons that match our input 
+
+//   filteredPokemons = () => {
+//     return this.props.pokemons.filter(pokemon => {
+//       return pokemon.name.includes(this.props.input.toLowerCase())
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <Card.Group itemsPerRow={6}>
+//         <h1>Hello From Pokemon Collection</h1>
+//         {this.renderAllPokemon()}
+//       </Card.Group>
+//     )
+//   }
+// }
+
+// export default PokemonCollection
+
+const PokemonCollection = (props) => {
 
   // takes an array of pokemons and renders 
-  renderAllPokemon = () => {
-    return this.filteredPokemons().map(pokemon => {
+  const renderAllPokemon = () => {
+    return filteredPokemons().map(pokemon => {
       return <PokemonCard pokemon={pokemon}/>
     })
   }
 
   // an array of pokemons that match our input 
 
-  filteredPokemons = () => {
-    return this.props.pokemons.filter(pokemon => {
-      return pokemon.name.includes(this.props.input.toLowerCase())
+  const filteredPokemons = () => {
+    return props.pokemons.filter(pokemon => {
+      return pokemon.name.includes(props.input.toLowerCase())
     })
   }
 
-  render() {
-    return (
-      <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-        {this.renderAllPokemon()}
-      </Card.Group>
-    )
-  }
+  return (
+    <Card.Group itemsPerRow={6}>
+      <h1>Hello From Pokemon Collection</h1>
+      {renderAllPokemon()}
+    </Card.Group>
+  )
 }
 
 export default PokemonCollection
